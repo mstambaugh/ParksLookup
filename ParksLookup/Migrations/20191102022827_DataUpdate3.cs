3fc7a5bd9586d0cd3efd2addec63305e528e94aa
Molly Stambaugh <molly.stambaugh@gmail.com>
2019-11-01 19:56:01 -0700

    complete data seeding and update readme documentation

---
+++ ParksLookup/Migrations/20191102022827_DataUpdate3.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ParksLookup.Migrations
+{
+    public partial class DataUpdate3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "NativeSpecies",
+                columns: new[] { "NativeSpeciesId", "Class", "CommonName", "ConservationStatus", "Kingdom", "ParkId", "Species", "SpeciesDescription" },
+                values: new object[,]
+                {
+                    { 2, "Mammalia", "Hoary Marmot", "least concern", "Animalia", 3, "Marmota caligata", "The hoary marmot is a large, bulky, ground squirrel, with short, heavy limbs, and a broad head. They frequently sun themselves on rocks, spending as much as 44% of their time in the morning doing so, although they will shelter in their burrows or otherwise seek shade in especially warm weather. They forage for the rest of the day, returning to their burrows to sleep during the night and  hibernate seven to eight months a year in burrows." },
+                    { 3, "Cataceae", "Grizzly Bear Prickly Pear Cactus", "least concern", "Plantae", 2, "Opuntia erinacea", "The Grizzly Bear Prickly Pear cactus forms small, low clumps with erect branches. The pads are thickly covered with long, gray, downward-pointing spines. Flower color varies from rose to yellow depending on variety and the fruit is dry and spiny." },
+                    { 4, "Reptilia", "Desert Tortoise", "vulnerable", "Animalia", 2, "Gopherus agassizii", "The desert tortoise lives about 50 to 80 years; it grows slowly and generally has a low reproductive rate. Desert tortoises can live in areas with ground temperatures exceeding 140 °F (60 °C) because of their ability to dig burrows and escape the heat. At least 95% of their lives are spent in burrows, which helps them to regulate body temperature and reduce water loss. They are most active after seasonal rains and are inactive during most of the year." },
+                    { 5, "Pinaceae", "White-barked Pine", "endangered", "Plantae", 2, "Pinus Albicaulis", "The whitebark pine is typically the highest-elevation pine tree found in Western US mountain ranges and often marks the tree line. Thus, it is often found as krummholz, trees growing close to the ground that have been dwarfed by exposure. In more favorable conditions, the trees may grow to 29 meters (95 ft) in height." }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "NativeSpecies",
+                keyColumn: "NativeSpeciesId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "NativeSpecies",
+                keyColumn: "NativeSpeciesId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "NativeSpecies",
+                keyColumn: "NativeSpeciesId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "NativeSpecies",
+                keyColumn: "NativeSpeciesId",
+                keyValue: 5);
+        }
+    }
+}
--- ParksLookup/README.md
+++ ParksLookup/README.md
@@ -1,4 +1,4 @@
-# _Parks Lookup_
+# _Parks and Native Species API_
 
 #### API in C#/.NET using EF Core, SQL, Pomelo, and Swagger for_**Epicodus**_
 
@@ -6,16 +6,15 @@
 
 ## Description
 
-_This API allows a developer to add, view, edit, and delete park entries in a locally hosted database_
+_This API allows a developer to add, view, update, and delete state and national parks, along with species native to particular parks. The API contains two rows storing data in a one-to-many object relationship, follows RESTful conventions, and utilizes MySQL Workbench._
 
 ## Setup/Installation Requirements
 
 * _Clone project from GitHub, navigate to directory, and open in a text editor_
 * _Open My SQL Workbench and create a new schema titled parks_lookup_
-* _Run dotnet restore_
-* _ _
-* _ _
-* _Run dotnet restore, dotnet build and dotnet run watch to host locally_
+* _Run-- dotnet restore, dotnet build, dotnet ef migrations add Initial, and dotnet ef database update --in the terminal_ 
+* _Use dotnet run or dotnet watch run to launch on local host_
+* _API modification beyond existing seeded data available through http://localhost:5000/swagger and MySQL_
 * _https://mstambaugh.github.io/ParksLookup_
 
 
